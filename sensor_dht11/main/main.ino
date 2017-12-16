21546697c889a8fe1569d4d407d95a49f580da5d
marcelo <m_lima@bitbucket.org>
2017-12-16 21:20:24 -0200

    [WIP] Adiciona skecth que realiza a leitura do leitor DHT11

---
+++ sensor_dht11/main/main.ino
@@ -0,0 +1,52 @@
+#include "DHT.h"
+
+#define DHTPIN A0 // pino que estamos conectado
+#define DHTTYPE DHT11 // DHT 11
+#define LED_PIN 14
+
+// Conecte pino 1 do sensor (esquerda) ao +5V
+// Conecte pino 2 do sensor ao pino de dados definido em seu Arduino
+// Conecte pino 4 do sensor ao GND
+// Conecte o resistor de 10K entre pin 2 (dados) 
+// e ao pino 1 (VCC) do sensor
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() 
+{
+  Serial.begin(9600);
+  Serial.println("DHT11 test!");
+  dht.begin();
+  pinMode(LED_PIN,OUTPUT);
+}
+
+void loop() 
+{
+  // A leitura da temperatura e umidade pode levar 250ms!
+  // O atraso do sensor pode chegar a 2 segundos.
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  // testa se retorno é valido, caso contrário algo está errado.
+  if (isnan(t) || isnan(h)) 
+  {
+    Serial.println("Failed to read from DHT");
+  } 
+  else 
+  {
+    Serial.print("Umidade: ");
+    Serial.print(h);
+    Serial.print(" %t");
+    Serial.print("Temperatura: ");
+    Serial.print(t);
+    Serial.println(" *C");
+  }
+  blinkLed();
+  delay(3000);
+}
+
+void blinkLed() {
+  digitalWrite(LED_PIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_PIN, LOW);
+  delay(1000);
+}
+
