03b30d4064c02fb77308e172ca906454dfa706e2
marcelo <m_lima@bitbucket.org>
2018-01-25 21:56:30 -0200

    Alteracoes no desafio

--- microgenios/projetos/03-AD/unit07/chalange.c
+++ microgenios/projetos/03-AD/unit07/chalange.c
@@ -26,6 +26,9 @@ sbit LCD_D7_Direction at TRISB3_bit;
 
 void configADC() {
  // Configuração do primeiro trimpot AN0
+ ANSELA.B0 = 1; // Somente RA0/AN0 como ADC;
+ ANSELC.B2 = 1; // Somente RC2/AN14 como ADC;
+ ANSELB = 0; // Como digital, para o LCD
  TRISA.RA0 = 1; // Entrada do trimpot, usando o AN0
  PORTA.RA0 = 1; // Opcional, estado alto
 
@@ -61,21 +64,15 @@ void main() {
  unsigned int leitura_ADC;
  unsigned char texto[10];
  unsigned char chanel = 0;
-
- #ifdef PIC18F45K22
-    ANSELA.A0 = 1; // Somente RA0/AN0 como ADC;
-    ANSELC.C2 = 1; // Somente RC2/AN14 como ADC;
-    ANSELB = 0; // Como digital
- #endif
-    
+  
  configADC();
  initLCD();
 
  while(1) {
   ADCON0.GO_DONE = 1; // Inicio do processo de conversão
-  WordToStr(leituraADC(chanel), texto);
+  WordToStr(leituraADC(0), texto);
   LCD_Out(chanel == 0 ? 1 : 2,10,Texto);
   Delay_ms(20);
-  chanel = !chanel;
+  //chanel = !chanel;
  }
 }
